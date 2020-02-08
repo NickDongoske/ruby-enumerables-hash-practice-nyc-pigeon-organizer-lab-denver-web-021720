3189b1146c576c9b8186cb23766820025aba8456
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-08 23:05:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |color_gender_lives, value|
-    value.each do |stats, names|
-      names.each do |name|
-        if pigeons[name] == nil
-          pigeons[name] = {}
+    value.each do |stats, all_names|
+      all_names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
         end
-        if pigeons[name][color_gender_lives] == nil
-          pigeons[name][color_gender_lives] = []
+        if pigeon_list[name][color_gender_lives] == nil
+          pigeon_list[name][color_gender_lives] = []
         end
-        pigeons[name][color_gender_lives].push(stats.to_s)
+        pigeon_list[name][color_gender_lives].push(stats.to_s)
       end
     end 
   end
