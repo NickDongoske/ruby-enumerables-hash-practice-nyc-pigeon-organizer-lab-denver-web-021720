961950dcd754fec4dfac8884e3482682427b088e
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-08 23:03:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,5 +6,5 @@ def nyc_pigeon_organizer(data)
         if pigeons[name] == nil
           pigeons[name] = {}
         end
-        if pigeons[name]
+        if pigeons[name][color_gender_lives]
 end
