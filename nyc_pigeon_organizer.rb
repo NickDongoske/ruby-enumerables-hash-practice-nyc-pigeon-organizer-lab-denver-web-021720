5b0c4c6518366ca1de65eaeb4b411ad4d0c4ee0c
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-08 23:02:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,5 @@ def nyc_pigeon_organizer(data)
   pigeons = {}
   data.each do |color_gender_lives, value|
     value.each do |stats, names|
-      names.each do |name
+      names.each do |name|
 end
